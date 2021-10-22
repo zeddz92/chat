1c174f4fb2f62b769515e516477fe1b0a205e706
Edd <reyes@agilemedia.jp>
2021-10-22 18:06:51 +0900

    Implement Sidepanel component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 
 export const Container: FC = ({ children }) => {
   return (
-    <div className="max-w-screen-xl w-full h-full mx-auto">{children}</div>
+    <div className="max-w-desktop w-full h-full mx-auto shadow-lg border border-primary-500">
+      {children}
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import { Container } from "../components/Container";
 import Head from "next/head";
 
-import { Chat } from "../components/Chat";
-import { Message } from "../components/Message";
-import { UsersMenu } from "../components/UsersMenu";
-import { channels } from "../constants";
-import { useContext } from "react";
-import { ChatContext } from "../contexts/ChatContext";
-import { Messages } from "../components/Messages";
+import { Container } from "../components/Container";
+
+import { SidePanel } from "../components/SidePanel/SidePanel";
 
 export default function Home() {
   const { channel, updateState } = useContext(ChatContext);
   return (
-    <div className="h-screen py-4">
+    <div className="h-screen py-6">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -20,22 +16,7 @@ export default function Home() {
 
       <Container>
         <div className="flex h-full">
-          <div className="w-3/5">
-            <UsersMenu />
-
-            <div className="px-4 w-full  mt-2">
-              <div className="text-sm text-gray-600 py-1">Channels</div>
-              {channels.map((ch) => (
-                <Chat
-                  name={ch.name}
-                  picture={ch.picture}
-                  selected={channel.id === ch.id}
-                />
-              ))}
-            </div>
-          </div>
-
-          <Messages />
+          <SidePanel />
         </div>
       </Container>
       {/* Messages component side/ */}
