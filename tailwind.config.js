b167f6bebf0fcfbb8e78e8397092c97e1b9796c8
Edd <reyes@agilemedia.jp>
2021-10-22 17:54:52 +0900

    fixup! Handle chat state

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: "#e2e4e5",
+          100: "#b7babe",
+          200: "#878d92",
+          300: "#565f66",
+          400: "#323c46",
+          500: "#0e1a25",
+          600: "#0c1721",
+          700: "#0a131b",
+          800: "#080f16",
+          900: "#04080d",
+        },
+      },
       maxWidth: {
+        sidePanel: "418px",
+        desktop: "1396px",
         "1/2": "50%",
       },
     },
