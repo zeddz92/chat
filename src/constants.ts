49a0382c4c4a7ca3f3eaa16c0765bc7ba4d1026f
Edd <reyes@agilemedia.jp>
2021-10-24 18:33:24 +0900

    Use images from local

--- src/constants.ts
+++ src/constants.ts
@@ -2,40 +2,34 @@ export const users = [
   {
     id: "Sam",
     name: "Sam",
-    picture:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSagAo_-Zyd9E2EcUyFERuN8CrSoBPs9fYFHQ&usqp=CAU",
+    picture: "/img/users/Sam.png",
   },
   {
     id: "Russell",
     name: "Russell",
-    picture:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAx0xs8hutsj14TS0mw8CWwlyzD7bE1pGXHGD3VOcWwdeae5qZKZol15_U5hhiA8bvgWU&usqp=CAU",
+    picture: "/img/users/Russell.png",
   },
   {
     id: "Joyse",
     name: "Joyse",
-    picture:
-      "https://t4.ftcdn.net/jpg/02/78/70/99/360_F_278709964_PhS3MsOE9udVYb5VCin1xCQJlm3HFb9V.jpg",
+    picture: "/img/users/Joyse.png",
   },
 ];
 
 export const channels = [
   {
     id: "1",
-    picture:
-      "https://d34u8crftukxnk.cloudfront.net/slackpress/prod/sites/6/2017-11_Haughey_EssentialChannels_FINAL_01_social.png",
+    picture: "/img/channels/General.png",
     name: "General Channel",
   },
   {
     id: "2",
-    picture:
-      "https://img4.goodfon.com/wallpaper/nbig/d/77/render-protsessor-robot-provereno-provereno-elektronikoi-you.jpg",
+    picture: "/img/channels/Technology.jpg",
     name: "Technology Channel",
   },
   {
     id: "3",
-    picture:
-      "https://images.indianexpress.com/2021/09/GettyImages-sun-star-science-teacher-explains-1200.jpg",
+    picture: "/img/channels/LGTM.jpg",
     name: "LGTM Channel",
   },
 ];
