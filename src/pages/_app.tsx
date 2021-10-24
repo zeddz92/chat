7df07b9df25d3f249ebf989601e413f1318387f8
Edd <reyes@agilemedia.jp>
2021-10-24 16:37:50 +0900

    Add uid package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/global.css";
 
 import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
+import { UIDReset } from "react-uid";
 
 import { ChatProvider } from "../contexts/ChatContext";
 import { useApollo } from "../graphql/apolloClient";
@@ -11,9 +12,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ApolloProvider client={apolloClient}>
-      <ChatProvider>
-        <Component {...pageProps} />
-      </ChatProvider>
+      <UIDReset>
+        <ChatProvider>
+          <Component {...pageProps} />
+        </ChatProvider>
+      </UIDReset>
     </ApolloProvider>
   );
 }
