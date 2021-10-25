616ab1e21370b7018744960b1b2967a329559bed
Edd <reyes@agilemedia.jp>
2021-10-25 19:23:10 +0900

    Set default value for text

--- src/utils/generateColor.ts
+++ src/utils/generateColor.ts
@@ -1,6 +1,6 @@
 const colors = ["#ED2F36", "#F3C724", "#4BAD4E", "#1196F5", "#FFB63D"];
 
-export const generateColor = (text: string) => {
+export const generateColor = (text = "") => {
   const sanitizedLetter = text.toUpperCase();
   const charCode = sanitizedLetter.charCodeAt(0);
 
